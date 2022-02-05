849fd77a0d5b7c1f9c5dc5b9c9ed5f21d2747465
nanaishere <87164398+nanaishere@users.noreply.github.com>
2022-02-05 17:11:28 +0300

    Update RadiomanTest.java

--- src/test/java/ru/netology/domain/RadiomanTest.java
+++ src/test/java/ru/netology/domain/RadiomanTest.java
@@ -1,178 +1,214 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadiomanTest {
 
-    @Test
-    void setMaxOneArgStation() {
-        Radioman radioman = new Radioman(10);
-
-        assertEquals(10, radioman.getMaxStation());
-
-    }
-
-    @Test
-    void setMaxNoArgsStation() {
+    @org.junit.jupiter.api.Test
+    void setMaxStation() {
         Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
+        radioman.setCurrentStation(5);
+        int excepted = 9;
+        assertEquals(excepted, radioman.getMaxStation());
 
-        assertEquals(9, radioman.getMaxStation());
     }
 
-    @Test
-    void SetMinNoArgsStation() {
+    @org.junit.jupiter.api.Test
+    void setMinStation() {
         Radioman radioman = new Radioman();
-
-        assertEquals(0, radioman.getMinStation());
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
+        radioman.setCurrentStation(2);
+        int excepted = 0;
+        assertEquals(excepted, radioman.getMinStation());
     }
 
-    @Test
-    void setMinArgsStation() {
-        Radioman radioman = new Radioman(9,0,5,100,0,50);
-
-        assertEquals(0, radioman.getMinStation());
-    }
-
-    @Test
-    void setCurrentArgsStation() {
-        Radioman radioman = new Radioman(5,30);
-
-        assertEquals(5, radioman.getCurrentStation());
-    }
-
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentStation() {
-        Radioman radioman = new Radioman(9,2,52);
-
-        assertEquals(2, radioman.getCurrentStation());
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
+        radioman.setCurrentStation(3);
+        int excepted = 3;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentStationOverMaxStation() {
-        Radioman radioman = new Radioman(9,5,22);
-        radioman.setCurrentStation(20);
-        assertEquals(9, radioman.getCurrentStation());
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
+        radioman.setCurrentStation(10);
+        int excepted = 9;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentStationEquallyMinStation() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
         radioman.setCurrentStation(0);
-        assertEquals(0, radioman.getCurrentStation());
+        int excepted = 0;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentStationUnderMinStation() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
-        radioman.setCurrentStation(-12);
-        assertEquals(0, radioman.getCurrentStation());
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
+        radioman.setCurrentStation(-1);
+        int excepted = 0;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setMaxVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
-
-        assertEquals(100, radioman.getMaxVolume());
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(4);
+        int excepted = 10;
+        assertEquals(excepted, radioman.getMaxVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setMinVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
-
-        assertEquals(0, radioman.getMinVolume());
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(8);
+        int excepted = 0;
+        assertEquals(excepted, radioman.getMinVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentVolume() {
-        Radioman radioman = new Radioman(9,7,25);
-
-        assertEquals(25, radioman.getCurrentVolume());
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(9);
+        int excepted = 9;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentVolumeOverMaxVolume() {
-        Radioman radioman = new Radioman(9,0,5,100,0,100);
-        radioman.setCurrentVolume(102);
-        assertEquals(100, radioman.getCurrentVolume());
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(12);
+        int excepted = 10;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentVolumeEquallyMinVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
         radioman.setCurrentVolume(0);
-        assertEquals(0, radioman.getCurrentVolume());
+        int excepted = 0;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCurrentVolumeUnderMinVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
         radioman.setCurrentVolume(-2);
-        assertEquals(0, radioman.getCurrentVolume());
+        int excepted = 0;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void upVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
-        radioman.setCurrentVolume(95);
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(5);
         radioman.upVolume();
-        assertEquals(96, radioman.getCurrentVolume());
+        int excepted = 6;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void upVolumeOverMaxVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
-        radioman.setCurrentVolume(105);
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
+        radioman.setCurrentVolume(12);
         radioman.upVolume();
-        assertEquals(100, radioman.getCurrentVolume());
+        int excepted = 10;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void downVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
         radioman.setCurrentVolume(5);
         radioman.downVolume();
-        assertEquals(4, radioman.getCurrentVolume());
+        int excepted = 4;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void downVolumeOverMinVolume() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxVolume(10);
+        radioman.setMinVolume(0);
         radioman.setCurrentVolume(-5);
         radioman.downVolume();
-        assertEquals(0, radioman.getCurrentVolume());
+        int excepted = 0;
+        assertEquals(excepted, radioman.getCurrentVolume());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void nextStation() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
         radioman.setCurrentStation(3);
         radioman.nextStation();
-        assertEquals(4, radioman.getCurrentStation());
+        int excepted = 4;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void nextStationAfterMaxStation() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
         radioman.setCurrentStation(10);
         radioman.nextStation();
-        assertEquals(0, radioman.getCurrentStation());
+        int excepted = 0;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void prevStation() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
         radioman.setCurrentStation(3);
         radioman.prevStation();
-        assertEquals(2, radioman.getCurrentStation());
+        int excepted = 2;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
-
-    @Test
+    @org.junit.jupiter.api.Test
     void prevStationAfterMinStation() {
-        Radioman radioman = new Radioman(9, 0, 5, 100, 0, 50);
+        Radioman radioman = new Radioman();
+        radioman.setMaxStation(9);
+        radioman.setMinStation(0);
         radioman.setCurrentStation(-1);
         radioman.prevStation();
-        assertEquals(9, radioman.getCurrentStation());
+        int excepted = 9;
+        assertEquals(excepted, radioman.getCurrentStation());
     }
 }
